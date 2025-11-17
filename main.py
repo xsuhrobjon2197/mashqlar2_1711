191f16069cc47bdedc3c110ccb343bda1d57b2d0
xsuhrobjon2197 <xsuhrobjon2197@gmail.com>
2025-11-17 02:49:36 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,75 @@
+#11-misol
+s = "abcdefgh"
+print(s)
+
+toza = s[1::2]
+print(toza)
+
+#12-misol
+s = "QWERTYUIOP"
+print(s)
+
+toza = s[::-1]
+print(toza)
+
+toza2 = toza[::2]
+print(toza2)
+
+#13-misol
+s = "abracadabra"
+print(s)
+
+toza = s[4:8]
+print(toza)
+
+#14-misol
+s = "abcdefghijk"
+print(s)
+
+toza = s[3:3]
+print(toza)
+
+#15-misol
+s = "GAMEOVERMISSION"
+print(s)
+
+toza = s[4:8]
+print(toza)
+
+#16-misol
+s = "123456789"
+print(s)
+
+toza = s[4:9]
+print(toza)
+
+toza2 = toza[::-1]
+print(toza2)
+
+#17-misol
+s = "indexning"
+print(s)
+
+toza = s[2:5]
+print(toza)
+
+#18-misol
+s = "slicingIsporeful"
+print(s)
+
+toza = s[3:]
+print(toza)
+
+#19-misol
+s = "abcdefghij"
+print(s)
+
+toza = s[-5:-2]
+print(toza)
+
+#20-misol
+s = "ABCDEFGHIJK"
+print(s)
+
+toza = s[-2:-8:-1]
+print(toza)
